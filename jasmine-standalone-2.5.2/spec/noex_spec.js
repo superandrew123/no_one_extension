272c581097b856bdf4d9f7745f744404703ab675
superandrew123 <andrewmiksch@gmail.com>
2016-11-22 10:46:04 -0500

    continuing set-up

--- jasmine-standalone-2.5.2/spec/noex_spec.js
+++ jasmine-standalone-2.5.2/spec/noex_spec.js
@@ -1,5 +1,5 @@
-describe('Add should add',()=>{
-  it('should add two numbers together',()=>{
+describe('Add should add', () => {
+  it('should add two numbers together', () => {
     expect(add(1,2)).toBe(3);
   });
 });
--- js/noex.js
+++ js/noex.js
@@ -1,3 +1,4 @@
 function add(x, y){
   return x + y;
 }
+  $('p');
